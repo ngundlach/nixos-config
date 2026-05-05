73dff008a9f49387c9ae55c6d463d0584a9a1e46
Nils Gundlach <11753635+ngundlach@users.noreply.github.com>
2026-05-05 23:24:30 +0200

    feat: adjusting wifi settings and enabling bluetooth

--- hosts/laptop/networking.nix
+++ hosts/laptop/networking.nix
@@ -1,11 +1,36 @@
 {config, pkgs, ...}:
 {
   networking.hostName = "maia";
-  networking.networkmanager.enable = true;
-  networking.networkmanager.wifi.backend = "iwd";
+  networking.networkmanager = {
+    enable = false;
+  };
+  networking.wireless = {
+    iwd = {
+      enable = true;
+      settings = {
+        General = {
+          EnableNetworkConfiguration = true;
+        };
+      };
+    };
+  };
 
   networking.nftables.enable = true;
   networking.firewall = {
     enable = true;
+  };
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = false;
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
   };
 }
