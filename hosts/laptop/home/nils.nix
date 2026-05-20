b119c05a6ada4d8d8abd41cbe6271fa42dcb7bd1
Nils Gundlach <11753635+ngundlach@users.noreply.github.com>
2026-05-20 22:30:20 +0200

    feat: adding hypridle config (and changing wallpaper)

--- hosts/laptop/home/nils.nix
+++ hosts/laptop/home/nils.nix
@@ -30,17 +30,45 @@
   in
     lib.concatLists (map optional modules);
 
+
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pidof swaylock || swaylock -c 000000";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })'";
+      };
+      listener = [
+        {
+          timeout = 300;
+          on-timeout = "hyprctl dispatch 'hl.dsp.dpms({ action = \"disable\" })'";
+          on-resume = "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })'";
+        }
+        {
+          timeout = 350;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 3600;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
+
   services.hyprpaper = {
     enable = true;
     settings = {
       splash = false;
       preload = [
-        "/home/nils/Pictures/train.jpg"
+        "/home/nils/Pictures/wp12329532-nixos-wallpapers.png"
       ];
       wallpaper = [
         {
           monitor = "";
-          path = "/home/nils/Pictures/train.jpg";
+          path = "/home/nils/Pictures/wp12329532-nixos-wallpapers.png";
+          fit_mode = "contain";
         }
       ];
     };
