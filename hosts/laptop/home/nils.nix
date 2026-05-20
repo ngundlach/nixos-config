b1e1deb25c7b0ee25d2f21c0281b019a8064461d
Nils Gundlach <11753635+ngundlach@users.noreply.github.com>
2026-05-20 22:24:43 +0200

    chore: refining main home-manager config
    removing code duplication and making all imports optional

--- hosts/laptop/home/nils.nix
+++ hosts/laptop/home/nils.nix
@@ -5,26 +5,30 @@
   home.stateVersion = "26.05";
 
   imports = let 
-      optional = path: lib.optional (builtins.pathExists path) path;
       p = ../../../home-modules;
-  in [
-      (p + "/nixvim.nix")
-      (p + "/themes.nix")
-      (p + "/tmux.nix")
-      (p + "/mpv.nix")
-      (p + "/zsh.nix")
-      (p + "/fuzzel.nix")
-      (p + "/yazi.nix")
-      (p + "/zoxide.nix")
-      (p + "/atuin.nix")
-      (p + "/ghostty.nix")
-      (p + "/eza.nix")
-      (p + "/starship.nix")
-      (p + "/fastfetch.nix")
-      (p + "/mime-apps.nix")
-      (p + "/alacritty.nix")
-    ] ++ optional (p +"/github.nix")
-      ++ optional (p +"/newsboat.nix");
+      mk = file: p + "/${file}";
+      optional = path: lib.optional (builtins.pathExists (mk path)) (mk path);
+      modules = [
+        "nixvim.nix"
+        "themes.nix"
+        "tmux.nix"
+        "mpv.nix"
+        "zsh.nix"
+        "fuzzel.nix"
+        "yazi.nix"
+        "zoxide.nix"
+        "atuin.nix"
+        "ghostty.nix"
+        "eza.nix"
+        "starship.nix"
+        "fastfetch.nix"
+        "mime-apps.nix"
+        "alacritty.nix"
+        "github.nix"
+        "newsboat.nix"
+      ];
+  in
+    lib.concatLists (map optional modules);
 
   services.hyprpaper = {
     enable = true;
