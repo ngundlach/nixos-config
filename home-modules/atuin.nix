cfac32894928cd8dd15f203ee38431a72f575185
Nils Gundlach <11753635+ngundlach@users.noreply.github.com>
2026-06-01 20:19:51 +0200

    chore: changing atuin config
    enabling daemon-mode

--- home-modules/atuin.nix
+++ home-modules/atuin.nix
@@ -2,9 +2,12 @@
 {
   programs.atuin = {
     enable = true;
+    daemon.enable = true;
     enableZshIntegration = true;
     enableNushellIntegration = true;
     settings = {
+      auto_sync = false;
+      search_mode = "daemon-fuzzy";
       inline_height = 15;
       enter_accept = true;
     };
