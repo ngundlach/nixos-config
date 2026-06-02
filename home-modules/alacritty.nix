dfc7b7c7d58b6020dee48d7c3f39e7501e27dc6c
Nils Gundlach <11753635+ngundlach@users.noreply.github.com>
2026-06-02 22:38:48 +0200

    chore: adjusting alacritty config
    adding font settings

--- home-modules/alacritty.nix
+++ home-modules/alacritty.nix
@@ -3,5 +3,14 @@
   programs.alacritty = {
     enable = true;
     theme = "tokyo_night";
+    settings = {
+      font.size = 12;
+      font = {
+        normal = {
+          family = "MesloLGS Nerd Font Mono";
+          style = "Regular";
+        };
+      };
+    };
   };
 }
