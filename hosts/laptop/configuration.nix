effa344f453b3841cffbcd66b9a11e3d63195187
Nils Gundlach <11753635+ngundlach@users.noreply.github.com>
2026-07-24 13:03:32 +0200

    feat: enabling auto-optimising of the nixstore

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -8,6 +8,7 @@
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    auto-optimise-store = true;
   };
 
   nixpkgs.config.allowUnfree = true;
