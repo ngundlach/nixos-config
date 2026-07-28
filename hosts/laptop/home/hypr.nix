9284788a865eae545f48aa2763199a3e66d8dafd
Nils Gundlach <11753635+ngundlach@users.noreply.github.com>
2026-07-28 16:26:09 +0200

    chore: moving hypr ecosystem configs to hypr.nix home-module

---
+++ hosts/laptop/home/hypr.nix
@@ -0,0 +1,46 @@
+_: {
+  services = {
+    hypridle = {
+      enable = true;
+      settings = {
+        general = {
+          lock_cmd = "pidof swaylock || swaylock -e -i ~/Pictures/locked.png -c 000000";
+          before_sleep_cmd = "loginctl lock-session";
+          after_sleep_cmd = "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })'";
+        };
+        listener = [
+          {
+            timeout = 300;
+            on-timeout = "hyprctl dispatch 'hl.dsp.dpms({ action = \"disable\" })'";
+            on-resume = "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })'";
+          }
+          {
+            timeout = 350;
+            on-timeout = "loginctl lock-session";
+          }
+          {
+            timeout = 3600;
+            on-timeout = "systemctl suspend";
+          }
+        ];
+      };
+    };
+
+    hyprpaper = {
+      enable = true;
+      settings = {
+        splash = false;
+        preload = [
+          "/home/nils/Pictures/jasa_scaled.jpg"
+        ];
+        wallpaper = [
+          {
+            monitor = "";
+            path = "/home/nils/Pictures/jasa_scaled.jpg";
+            fit_mode = "contain";
+          }
+        ];
+      };
+    };
+  };
+}
--- hosts/laptop/home/nils.nix
+++ hosts/laptop/home/nils.nix
@@ -35,52 +35,10 @@
     ];
     hostModules = map (optional ./.) [
       "ssh.nix"
+      "hypr.nix"
     ];
   in
     lib.concatLists (sharedModules ++ hostModules);
-
-  services.hypridle = {
-    enable = true;
-    settings = {
-      general = {
-        lock_cmd = "pidof swaylock || swaylock -e -i ~/Pictures/locked.png -c 000000";
-        before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })'";
-      };
-      listener = [
-        {
-          timeout = 300;
-          on-timeout = "hyprctl dispatch 'hl.dsp.dpms({ action = \"disable\" })'";
-          on-resume = "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })'";
-        }
-        {
-          timeout = 350;
-          on-timeout = "loginctl lock-session";
-        }
-        {
-          timeout = 3600;
-          on-timeout = "systemctl suspend";
-        }
-      ];
-    };
-  };
-
-  services.hyprpaper = {
-    enable = true;
-    settings = {
-      splash = false;
-      preload = [
-        "/home/nils/Pictures/jasa_scaled.jpg"
-      ];
-      wallpaper = [
-        {
-          monitor = "";
-          path = "/home/nils/Pictures/jasa_scaled.jpg";
-          fit_mode = "contain";
-        }
-      ];
-    };
-  };
 
   programs = {
     direnv = {
