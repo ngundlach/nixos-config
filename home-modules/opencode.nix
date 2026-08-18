4930738452942eb76438e6be30ef8898b6ae6e24
Nils Gundlach <11753635+ngundlach@users.noreply.github.com>
2026-08-18 12:46:45 +0200

    chore(opencode): moving mpc-nixos from global to project specific config

--- home-modules/opencode.nix
+++ home-modules/opencode.nix
@@ -1,11 +1,9 @@
-{pkgs, ...}: {
+_: {
   programs.opencode = {
     enable = true;
-    settings.mcp.nixos = {
-      type = "local";
-      command = ["${pkgs.mcp-nixos}/bin/mcp-nixos"];
-      enabled = true;
+    tui = {
+      theme = "tokyonight";
+      attention.enabled = true;
     };
-    tui.theme = "tokyonight";
   };
 }
